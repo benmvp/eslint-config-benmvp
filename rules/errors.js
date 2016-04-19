e7eb9d4bbf75885b1244c88cca01932e02219d88
Ben Ilegbodu <ben@benmvp.com>
2016-04-18 21:49:00 -0700

    Relax no-console to warn

--- rules/errors.js
+++ rules/errors.js
@@ -1,5 +1,9 @@
 module.exports = {
     'rules': {
+        // warn when using console
+        // http://eslint.org/docs/rules/no-console
+        'no-console': 'warn',
+
         // disallow declaration of variables already declared in the outer scope
         // http://eslint.org/docs/rules/no-shadow
         'no-shadow': 'error',
